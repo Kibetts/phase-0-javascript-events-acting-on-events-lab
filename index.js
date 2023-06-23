40ffae16f8110181fd9e3ade70ed6db8cfe71f12
Brian Kibet <kibetsbrian@gmail.com>
2023-06-23 17:55:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Your code here
 function moveDodgerLeft(){
-    
+    function moveDodgerLeft() {
+        let dodger = document.getElementById('dodger');
+        let left = parseInt(dodger.style.left);
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`;
+        }
+      }
 }
